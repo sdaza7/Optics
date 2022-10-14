a0a1f3277588e46c85910d299c11bcee2e71805c
Samuel DAza <sdaza@unal.edu.com>
2022-10-13 19:00:42 -0500

    Se guardan valores del getinput

--- ImProcess1.py
+++ ImProcess1.py
@@ -1,10 +1,16 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-
+import numpy as np
 #Importar Imagen
 img = mpimg.imread('IM1.jpeg')
 #Mostrar la Imagen
 plt.imshow(img,cmap='gray')
 #Seleccionar un punto
 u = plt.ginput(1)
-print(u)
+print(np.shape(u))
+#pixel=img[u]
+print(u[0])
+u1, u2 = u[0]
+print(u2)
+IR = img[50:100,100:200]
+plt.imshow(IR,cmap='gray')
